c028be726edc89968f0aea2eb9aa93d6e6665fdd
2power9 <huenhu0512@gmail.com>
2022-05-26 06:17:51 +0700

    feat: add atributes and methods for beverages

--- src/Beverage/BaseBeverage.java
+++ src/Beverage/BaseBeverage.java
@@ -3,6 +3,7 @@ package Beverage;
 public abstract class BaseBeverage {
     protected String name;
     protected int price;
+    protected int limitToppings = 5;
 
     public int getPrice() {
         return price;
@@ -16,4 +17,9 @@ public abstract class BaseBeverage {
     public String toString() {
         return name;
     }
+
+    public int getLimit() {
+        return limitToppings;
+    }
+    public void setAmount(int num) {}
 }
