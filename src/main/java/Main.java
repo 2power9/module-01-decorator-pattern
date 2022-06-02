8b65ac9912a16eabf6a56b3c5176f62b531b5238
2power9 <huenhu0512@gmail.com>
2022-06-02 14:30:58 +0700

    BREAKING CHANGE: change code in main to get more orders

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,8 +1,19 @@
 import BeverageManager.BeverageManager;
+import Helper.InputHelper;
 
 public class Main {
     public static void main(String[] args) {
-        BeverageManager manager = new BeverageManager();
-        manager.order();
+        InputHelper in = new InputHelper(System.in);
+        while (true) {
+            BeverageManager manager = new BeverageManager();
+            manager.order();
+            System.out.println("======================================================");
+            System.out.println("======================================================");
+            System.out.println("======================================================");
+            System.out.println("New order? (0 - yes | 1 - no): ");
+            int op = Integer.parseInt(in.input());
+            if (op == 1) break;
+        }
+        System.out.println("Goodbye!!!");
     }
 }
